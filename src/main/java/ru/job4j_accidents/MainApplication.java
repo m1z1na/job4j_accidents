ed878fadaa49a05323cdb4b15d7c8e0d47306529
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-18 17:34:02 +0300

    0. Spring MVC [#6877  [#6877]]

---
+++ src/main/java/ru/job4j_accidents/MainApplication.java
@@ -0,0 +1,10 @@
+package ru.job4j_accidents;
+
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ src/main/java/ru/job4j_accidents/controller/IndexController.java
@@ -0,0 +1,6 @@
+package ru.job4j_accidents.controller;
+
+public class IndexController {
+
+
+}
