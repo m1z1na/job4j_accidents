32d1b98604c0bfbdc6b8f4b0c57b84c3b996dc41
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-18 20:00:33 +0300

    1. Загрузка данных в вид [#295782  [#295782]]

--- src/main/java/ru/job4j_accidents/MainApplication.java
+++ src/main/java/ru/job4j_accidents/MainApplication.java
@@ -1,10 +1,14 @@
 package ru.job4j_accidents;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MainApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
     }
 
-}
 
+}
