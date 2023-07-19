3a7c9ac9e01c617c8eceb2004f66d94b073a98f4
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-19 20:21:31 +0300

    1. Spring ORM [#2093  [#2093]]

--- src/main/java/ru/job4j/accidents/model/Accident.java
+++ src/main/java/ru/job4j/accidents/model/Accident.java
@@ -7,17 +7,28 @@ import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "accidents")
 public class Accident {
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String text;
-    private String address;
-    private AccidentType type;
-    private Set<Rule> rules;
+    private transient String text;
+    private transient String address;
+
+    private transient AccidentType type;
+    private transient Set<Rule> rules;
 
 }
