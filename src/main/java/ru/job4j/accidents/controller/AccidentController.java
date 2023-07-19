d7d2c6a9349dc41dacea2971808158fc948ecf8a
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-19 16:42:28 +0300

    3.1. @RequestParam. Форма редактирования [#308708  [#308708]]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -3,7 +3,10 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.IAccidentService;
 
@@ -17,16 +20,19 @@ public class AccidentController {
         return "createAccident";
     }
 
-    @GetMapping("/editAccident/{id}")
-    public String edit(Model model, @PathVariable int id) {
+
+    @GetMapping("/formUpdateAccident")
+    public String edit(@RequestParam("id") int id, Model model) {
         model.addAttribute("accident", this.accidents.getById(id));
         return "editAccident";
     }
+
     @PostMapping("/saveAccident")
     public String save(@ModelAttribute Accident accident) {
         accidents.create(accident);
         return "redirect:/index";
     }
+
     @PostMapping("/editAccident")
     public String update(@ModelAttribute Accident accident) {
         accidents.update(accident);
