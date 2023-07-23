1dd078254bb9a6db360f32534bd54b07118a5f9d
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-07-23 09:44:15 +0300

    4. Form с композиционным объектом [#305522  [#305522 #430935]]

--- src/main/java/ru/job4j/accidents/service/AccidentService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -8,6 +8,7 @@ import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.repository.AccidentMem;
 import ru.job4j.accidents.repository.AccidentTypeMem;
+import ru.job4j.accidents.repository.RuleMem;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,23 +19,23 @@ public class AccidentService implements IAccidentService {
 
     private final AccidentMem memory;
 
-    private final IRuleService rule;
-    private final IAccidentTypeService type;
+    private final RuleMem rule;
+    private final AccidentTypeMem type;
 
     /*private final AccidentJdbcTemplate accidentsRepostiory;*/
 
     @Autowired
-    public AccidentService(AccidentMem memory, AccidentTypeMem memoryType, IRuleService rule, IAccidentTypeService type) {
+    public AccidentService(AccidentMem memory, AccidentTypeMem memoryType, RuleMem rule) {
         this.memory = memory;
         this.rule = rule;
-        this.type = type;
+        this.type = memoryType;
         /*  this.accidentsRepostiory = accidentsRepostiory;*/
 
-        create(new Accident(1, "name1", "text1", "address1", this.type.getById(1), this.rule.findAll()));
-        create(new Accident(2, "name2", "text2", "address2", this.type.getById(2), this.rule.findAll()));
-        create(new Accident(3, "name3", "text3", "address3", this.type.getById(1), this.rule.findAll()));
-        create(new Accident(4, "name4", "text4", "address4", this.type.getById(1), this.rule.findAll()));
-        create(new Accident(5, "name5", "text5", "address5", this.type.getById(2), this.rule.findAll()));
+        create(new Accident(1, "name1", "text1", "address1", this.type.findById(1), this.rule.getAll()));
+        create(new Accident(2, "name2", "text2", "address2", this.type.findById(2), this.rule.getAll()));
+        create(new Accident(3, "name3", "text3", "address3", this.type.findById(1), this.rule.getAll()));
+        create(new Accident(4, "name4", "text4", "address4", this.type.findById(1), this.rule.getAll()));
+        create(new Accident(5, "name5", "text5", "address5", this.type.findById(2), this.rule.getAll()));
 
     }
 
@@ -53,12 +54,12 @@ public class AccidentService implements IAccidentService {
     @Override
     public List<AccidentType> findAllTypes() {
 
-        return type.findAll();
+        return type.getAll();
     }
 
     @Override
     public Set<Rule> findAllRules() {
-        return rule.findAll();
+        return rule.getAll();
     }
 
     @Override
